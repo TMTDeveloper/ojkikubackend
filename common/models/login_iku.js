c9fa30b6468ff30a192bc093a61a30ead5a1723a
rezaaulia <zenhuwmank@gmail.com>
2018-03-10 20:34:48 +0700

    barulogin

---
+++ common/models/login_iku.js
@@ -0,0 +1,82 @@
+var util = require('util');
+var jwt = require('jsonwebtoken')
+
+'use strict';
+
+module.exports = function (Loginiku) {
+  var app = require('../../server/server');
+  // var Loginiku = app.models.User;
+
+  //   app.post( function (req, res) {
+
+  // 	//parse user credentials from request body
+
+var secretKey = {
+  'secret': 'ilovescotchyscotch'
+}
+
+
+
+
+
+  Loginiku.loginuser = function (req, cb) {
+    console.log(JSON.stringify(req))
+    Loginiku.find({
+      where: {
+        USERNAME: req.email,
+        PASSWORD: req.password
+      }
+    }, (err, res) => {
+      if (util.isNullOrUndefined(res[0])) {
+        var data = {
+          name: "error",
+          status: "401",
+          message: "Wrong user/password"
+        }
+        cb(data)
+
+      } else {
+          console.log(res[0]);
+          var payload = res[0];
+        var token = jwt.sign(payload.toJSON(), secretKey.secret, {
+            expiresIn: '1h' // expires in 24 hours
+          });
+          var data={
+              token:token,
+          }
+        cb(null, data);
+      }
+    })
+
+
+    //   if (err) {
+    //     //custom logger
+    //     Log.error(err);
+    //     res.status(401).json({
+    //       "error": "login failed"
+    //     });
+    //   }
+
+  };
+
+
+  Loginiku.remoteMethod(
+    'loginuser', {
+      accepts: [
+          {
+        arg: 'user',
+        type: 'Object',
+        http: { source: 'body' }
+      }
+    ],
+      http: {
+        path: '/login',
+        verb: 'post'
+      },
+      returns: {
+        arg: 'data',
+        type: 'Object'
+      }
+    }
+  )
+};
