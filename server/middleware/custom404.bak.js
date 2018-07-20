a5d33a997c290f7ac2055c703f4fea0445025bec
Luqmanul Hakim <arlhba@gmail.com>
2018-07-20 23:47:39 +0700

    ww

--- server/middleware/custom404.bak.js
+++ server/middleware/custom404.bak.js
@@ -2,7 +2,7 @@
 module.exports = function() {
   var path = require("path");
   return function urlNotFound(req, res, next) {
-    let angular_index = path.resolve("client/index.html");
+    let angular_index = path.resolve("../client/index.html");
     res.sendFile(angular_index, function(err) {
       if (err) {
         console.error(err);
